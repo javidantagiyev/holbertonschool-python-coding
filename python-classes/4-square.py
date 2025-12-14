dfed2564afbc8ce98738969783cfa836d11bb65b
Javidan Taghiyev <jtaghiyev16178@ada.edu.az>
2025-12-14 17:14:34 +0000

    Task: 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+"""This module defines a Square class."""
+
+
 class Square:
+    """This class defines a square."""
+
     def __init__(self, size=0):
         self.size = size
 
     @property
     def size(self):
+        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -17,9 +24,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return the area of the square."""
         return self.__size * self.__size
 
     def my_print(self):
+        """Print the square using # characters."""
         if self.__size == 0:
             print()
             return
